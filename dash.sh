8354eca5a964bc4265d2e0bd4c94d685e5db9856
Abolfazl Esmailinejad <abolfazl.esm@gmail.com>
2019-08-07 16:58:24 +0430

    Add dash playlist generator bash script

--- dash.sh
+++ dash.sh
@@ -1,87 +1,79 @@
 #!/bin/bash
 
-MOVIE_DIR=$(pwd)
+# THIS SCRIPT CONVERTS EVERY MP4 (IN THE CURRENT) TO A MULTI-BITRATE VIDEO IN MP4-DASH
+# For each file "videoname.mp4", rename it based on file counter and creates a folder containing a dash manifest file "stream.mpd" and subfolders containing video segments.
+# ALSO, MAKE THUMNAILS FOR EVERY MP4 FILES THAT PREVIOSLEY CONVERTED TO DASH PLAYLIST
+
+# Add the following mime-types (uncommented) to .htaccess:
+# AddType video/mp4 m4s
+# AddType application/dash+xml mpd
+
+MYDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 THUMBS_DIR="thumbs"
 QUEUE_DIR="queue"
 PROCESSED_DIR="processed"
-REPORTED_DIR="reported"
-SUBTITLES_DIR="subtitles"
 DIR_CHUNK=1000
 COUNTER_FILE="counter.txt"
-REPORTING_URL="http://127.0.0.1:8000"
-QUALITY=(1080 720 480 360)
-Q1080=('1080' '9000k' '4500k')
-Q720=('720' '5000k' '2500k')
-Q480=('480' '2500k' '1250k')
-Q360=('360' '1500k' '700k')
+SUBTITLES_DIR="subtitles"
 
 # check programs
-if [[ -z "$(which ffmpeg)" ]]; then
-  echo "Exception: ffmpeg is not installed"
+if [ -z "$(which ffmpeg)" ]; then
+  echo "Error: ffmpeg is not installed"
   exit 1
 fi
 
-if [[ -z "$(which MP4Box)" ]]; then
-  echo "Exception: MP4Box is not installed"
+if [ -z "$(which MP4Box)" ]; then
+  echo "Error: MP4Box is not installed"
   exit 1
 fi
 
-if [[ -z "$(which montage)" ]]; then
-  echo "Exception: montage is not installed"
+if [ -z "$(which montage)" ]; then
+  echo "Error: montage is not installed"
   exit 1
 fi
 
-if [[ -z "$(which sed)" ]]; then
-  echo "Exception: sed is not installed"
+if [ -z "$(which xmlstarlet)" ]; then
+  echo "Error: xmlstarlet is not installed"
   exit 1
 fi
 
-if [[ -z "$(which curl)" ]]; then
-  echo "Exception: curl is not installed"
-  exit 1
-fi
-
-cd "$MOVIE_DIR" || exit
+cd "$MYDIR" || exit
 
 # check directories
-if [[ ! -d "${PROCESSED_DIR}" ]]; then
+if [ ! -d "${PROCESSED_DIR}" ]; then
   mkdir "${PROCESSED_DIR}"
 fi
 
-if [[ ! -d "${REPORTED_DIR}" ]]; then
-  mkdir "${REPORTED_DIR}"
-fi
-
-if [[ ! -d "${QUEUE_DIR}" ]]; then
+if [ ! -d "${QUEUE_DIR}" ]; then
   mkdir "${QUEUE_DIR}"
 fi
 
-if [[ ! -f "${COUNTER_FILE}" ]]; then
+if [ -f "${COUNTER_FILE}" ]; then
+  COUNTER=$(<"${COUNTER_FILE}")
+  SAVE_DIR=$(($((COUNTER / DIR_CHUNK)) + 1))
+else
   echo 0 >"${COUNTER_FILE}"
   COUNTER=0
   SAVE_DIR="1"
 fi
 
+if [ ! -d "${PROCESSED_DIR}/${SAVE_DIR}" ]; then
+  mkdir "${PROCESSED_DIR}/${SAVE_DIR}"
+fi
+
 # find mp4 files
 TARGET_FILES=$(find ./ -maxdepth 1 -type f \( -name "*.mp4" \))
 
-for f in ${TARGET_FILES}; do
-  COUNTER=$(<"${COUNTER_FILE}")
-  SAVE_DIR=$(($((COUNTER / DIR_CHUNK)) + 1))
-
-  if [[ ! -d "${REPORTED_DIR}/${SAVE_DIR}" ]]; then
-    mkdir "${REPORTED_DIR}/${SAVE_DIR}"
-  fi
-
-  ORIGINAL_FILE_FULL_NAME=$(basename "$f")           # full ame of the file
+for f in $TARGET_FILES; do
+  ORIGINAL_FILE_FULL_NAME=$(basename "$f") # fullname of the file
   ORIGINAL_FILE_NAME="${ORIGINAL_FILE_FULL_NAME%.*}" # name without extension
 
   FILE_NAME=$((COUNTER + 1))
   MP4="${QUEUE_DIR}/${FILE_NAME}.mp4"
   DASH_DIR="${QUEUE_DIR}/${FILE_NAME}"
 
-  # if DASH directory does not exist then convert
-  if [[ ! -d "${DASH_DIR}" ]]; then
+  # if DASH directory does not exist, convert
+  if [ ! -d "${DASH_DIR}" ]; then
     mkdir "${DASH_DIR}"
 
     # save original video file info
@@ -91,42 +83,27 @@ for f in ${TARGET_FILES}; do
     mv "${f}" "${QUEUE_DIR}/${FILE_NAME}.mp4"
     echo "Converting \"$f\" to multi-bitrate video in MPEG-DASH"
 
-    # audio channels count
-    AUDIO_CHANNELS=$(ffmpeg -i "${MP4}" 2>&1 | grep Audio | wc -l)
-    AUDIO_FILES=""
+    ffmpeg -y -i "${MP4}" -c:a aac -b:a 192k -vn "${DASH_DIR}/${FILE_NAME}_audio.m4a"
+    ffmpeg -y -i "${MP4}" -preset ultrafast -tune film -vsync passthrough -write_tmcd 0 -an -c:v libx264 -x264opts 'keyint=25:min-keyint=25:no-scenecut' -crf 23 -maxrate 1500k -bufsize 3000k -pix_fmt yuv420p -vf "scale=-2:1080" -f mp4 "${FILE_NAME}_1080.mp4"
+    ffmpeg -y -i "${MP4}" -preset ultrafast -tune film -vsync passthrough -write_tmcd 0 -an -c:v libx264 -x264opts 'keyint=25:min-keyint=25:no-scenecut' -crf 23 -maxrate 800k -bufsize 2000k -pix_fmt yuv420p -vf "scale=-2:720" -f mp4 "${FILE_NAME}_720.mp4"
+    ffmpeg -y -i "${MP4}" -preset ultrafast -tune film -vsync passthrough -write_tmcd 0 -an -c:v libx264 -x264opts 'keyint=25:min-keyint=25:no-scenecut' -crf 23 -maxrate 400k -bufsize 1000k -pix_fmt yuv420p -vf "scale=-2:480" -f mp4 "${FILE_NAME}_480.mp4"
+    # static file for ios and old browsers and mobile safari
+    # ffmpeg -y -i "${MP4}" -preset ultrafast -tune film -movflags +faststart -vsync passthrough -write_tmcd 0 -c:a aac -b:a 160k -c:v libx264 -crf 23 -maxrate 2000k -bufsize 4000k -pix_fmt yuv420p -f mp4 "${DASH_DIR}/${FILE_NAME}.mp4"
 
-    for ((i = 1; i <= AUDIO_CHANNELS; i++)); do
-      ffmpeg -y -i "${MP4}" -map "0:${i}" -c:a aac -ar 48000 -b:a 128k -vn "${FILE_NAME}_audio_$i.m4a"
-      AUDIO_FILES+="${FILE_NAME}_audio_$i.m4a "
-    done
-
-    HEIGHT=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nw=1:nk=1 ${MP4})
-    VIDEO_FILES=""
-
-    for q in "${QUALITY[@]}"; do
-      if [[ ! HEIGHT > q ]]; then
-        SCALE=Q$q[0]
-        SCALE=${!SCALE}
-        BUFFER=Q$q[1]
-        BUFFER=${!BUFFER}
-        BITRATE=Q$q[2]
-        BITRATE=${!BITRATE}
-
-        ffmpeg -hide_banner -y -i "${MP4}" \
-          -preset ultrafast -tune film -vsync passthrough -write_tmcd 0 -an -c:v libx264 -profile:v main \
-          -x264opts 'keyint=25:min-keyint=25:no-scenecut' -crf 23 \
-          -maxrate ${BITRATE} -bufsize ${BUFFER} -pix_fmt yuv420p -vf "scale=-2:${SCALE}" -f mp4 "${FILE_NAME}_${SCALE}.mp4"
-
-        VIDEO_FILES+="${FILE_NAME}_${SCALE}.mp4 "
-      fi
-    done
-
-    MP4Box -dash 2000 -rap -frag-rap -bs-switching no -profile "dashavc264:live" ${VIDEO_FILES} ${AUDIO_FILES} -out "${DASH_DIR}/${FILE_NAME}.mpd"
-    rm ${VIDEO_FILES} ${AUDIO_FILES}
+    # if audio stream does not exist, ignore it
+    if [ -e "${FILE_NAME}_audio.m4a" ]; then
+      MP4Box -dash 2000 -rap -frag-rap -bs-switching no -profile "dashavc264:live" "${FILE_NAME}_1080.mp4" "${FILE_NAME}_720.mp4" "${FILE_NAME}_480.mp4" "${FILE_NAME}_audio.m4a" -out "${DASH_DIR}/${FILE_NAME}.mpd"
+      rm "${FILE_NAME}_1080.mp4" "${FILE_NAME}_720.mp4" "${FILE_NAME}_480.mp4" "${FILE_NAME}_audio.m4a"
+    else
+      MP4Box -dash 2000 -rap -frag-rap -bs-switching no -profile "dashavc264:live" "${FILE_NAME}_1080.mp4" "${FILE_NAME}_720.mp4" "${FILE_NAME}_480.mp4" -out "${DASH_DIR}/${FILE_NAME}.mpd"
+      rm "${FILE_NAME}_1080.mp4" "${FILE_NAME}_720.mp4" "${FILE_NAME}_480.mp4"
+    fi
   fi
 
-  # if thumbs directory does not exist then make thumbs
-  if [[ ! -d "${DASH_DIR}/${THUMBS_DIR}" ]]; then
+  # TODO: CHECK THAT LAST CHUNK HAS MORE THAN 0 BYTE
+
+  # if thumbs directory does not exist, make thumbs
+  if [ ! -d "${DASH_DIR}/${THUMBS_DIR}" ]; then
     mkdir "${DASH_DIR}/${THUMBS_DIR}"
     echo "Making thumbnails for \"$f\""
     ffmpeg -i "${MP4}" -s 160x90 -vf fps=1/10 "${DASH_DIR}/${THUMBS_DIR}/thumb-%04d.jpg"
@@ -136,13 +113,13 @@ for f in ${TARGET_FILES}; do
   # find all srt files that have same name of original video
   SUB_FILES=$(find ./ -maxdepth 1 -type f \( -name "${ORIGINAL_FILE_NAME}_*.srt" \))
 
-  if [[ ! -d "${DASH_DIR}/${SUBTITLES_DIR}" ]]; then
+  if [ ! -d "${DASH_DIR}/${SUBTITLES_DIR}" ]; then
     mkdir "${DASH_DIR}/${SUBTITLES_DIR}"
   fi
 
-  for subtitle in ${SUB_FILES}; do
+  for subtitle in $SUB_FILES; do
     SUBTITLE_NAME="${subtitle%.*}" # name without extension
-    # get last two char of srt file as subtitle language (based on default convention, e.g. movie_EN.srt)
+    # get last two char of srt file as subtitle language (based on default condition)
     SUB_LANG=${SUBTITLE_NAME:(-2)}
     # convert srt to vtt
     ffmpeg -i "${subtitle}" "${FILE_NAME}_${SUB_LANG}.vtt"
@@ -152,31 +129,22 @@ for f in ${TARGET_FILES}; do
     rm "${subtitle}"
 
     # append subtitle node to DASH mpd file
-    APPEND="<AdaptationSet mimeType=\"text\/vtt\" lang=\"${SUB_LANG}\"><Representation id=\"caption_${SUB_LANG}\" bandwidth=\"256\"><BaseURL>${SUBTITLES_DIR}/${FILE_NAME}_${SUB_LANG}.vtt<\/BaseURL><\/Representation><\/AdaptationSet>"
-    sed -i "/<\/Period>/i $APPEND" "${DASH_DIR}/${FILE_NAME}.mpd"
+    xml ed --inplace -P -O \
+      -s "//MPD/Period" -t elem -n "AdaptationSet" \
+      -s "//MPD/Period/AdaptationSet[last()]" -t attr -n "mimeType" -v "text/vtt" \
+      -s "//MPD/Period/AdaptationSet[last()]" -t attr -n 'lang' -v "${SUB_LANG}" \
+      -s "//MPD/Period/AdaptationSet[last()]" -t elem -n "Representation" \
+      -s "//MPD/Period/AdaptationSet/Representation[last()]" -t attr -n "caption_${SUB_LANG}" -v "text/vtt" \
+      -s "//MPD/Period/AdaptationSet/Representation[last()]" -t attr -n "bandwidth" -v "256" \
+      -s "//MPD/Period/AdaptationSet/Representation[last()]" -t elem -n "BaseURL" -v "${SUBTITLES_DIR}/${FILE_NAME}_${SUB_LANG}.vtt" \
+      "${DASH_DIR}/${FILE_NAME}.mpd"
   done
 
   # if preview sprite generated, move DASH to processed directory and increase counter
-  if [[ -f "${DASH_DIR}/${THUMBS_DIR}/preview.jpg" ]]; then
-    mv "${DASH_DIR}" "${PROCESSED_DIR}"
-    mv "${MP4}" "${PROCESSED_DIR}"
-    rm "${MP4}"
+  #if [ -f "${DASH_DIR}/${THUMBS_DIR}/preview.jpg" ]; then
+    mv "${DASH_DIR}" "${PROCESSED_DIR}/${SAVE_DIR}"
+    mv "${MP4}" "${PROCESSED_DIR}/${SAVE_DIR}"
     COUNTER=$((COUNTER + 1))
     echo ${COUNTER} >"${COUNTER_FILE}"
-
-    # curl service to insert movie into database
-    STATUS=$(curl -i -X POST \
-      -F "name=${FILE_NAME}" \
-      -F "manifest=${SAVE_DIR}/${FILE_NAME}/${FILE_NAME}.mpd" \
-      -F "preview=${SAVE_DIR}/${FILE_NAME}/${THUMBS_DIR}/preview.jpg" \
-      -F "info=@${PROCESSED_DIR}/${FILE_NAME}/info.txt" \
-      --url ${REPORTING_URL} \
-      --output "${PROCESSED_DIR}/${FILE_NAME}/response.txt" -w "%{http_code}")
-
-    # if response header code is 200, move to reported directory
-    if [[ "$STATUS" -eq 200 ]]; then
-      mv "${PROCESSED_DIR}/${FILE_NAME}" "${REPORTED_DIR}/${SAVE_DIR}"
-      mv "${PROCESSED_DIR}/${FILE_NAME}.mp4" "${REPORTED_DIR}/${SAVE_DIR}"
-    fi
-  fi
+  #fi
 done
